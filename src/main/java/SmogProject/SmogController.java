a33b42bea24f1eb14ec21727029c1ebb883931f9
Grzegorz Opoka <grzego.opoka@live.com>
2016-06-07 13:30:50 +0200

    Removed unused view parameter.

--- src/main/java/SmogProject/SmogController.java
+++ src/main/java/SmogProject/SmogController.java
@@ -27,74 +27,46 @@ public class SmogController {
     public @ResponseBody
     List<SmogEntity> smog(@RequestParam(value="from", defaultValue="2016-04-06") String from,
                           @RequestParam(value="to", defaultValue="2016-04-06") String to,
-                          @RequestParam(value="view", defaultValue="normal") String view,
                           @RequestParam(value = "station", defaultValue = "7") String station) throws ParseException {
 
-        if (Objects.equals(view, "normal")) {
-            Date fromdate = new SimpleDateFormat("yyyy-MM-dd").parse(from);
-            Date todate = new SimpleDateFormat("yyyy-MM-dd").parse(to);
+        Date fromdate = new SimpleDateFormat("yyyy-MM-dd").parse(from);
+        Date todate = new SimpleDateFormat("yyyy-MM-dd").parse(to);
 
-            long diff = todate.getTime() - fromdate.getTime() + TimeUnit.DAYS.toMillis(1);
-            long secs = TimeUnit.MILLISECONDS.toSeconds(diff) / MAX_POINTS;
-            long divhours = TimeUnit.MILLISECONDS.toHours(diff) / MAX_POINTS + 1;
+        long diff = todate.getTime() - fromdate.getTime() + TimeUnit.DAYS.toMillis(1);
+        long secs = TimeUnit.MILLISECONDS.toSeconds(diff) / MAX_POINTS;
+        long divhours = TimeUnit.MILLISECONDS.toHours(diff) / MAX_POINTS + 1;
 
-            String label;
-            if (divhours < 24) { // hours
-                label = "P.godzina || ':00'";
-            } else { // days, month
-                label = "P.data";
-            }
-
-            String query = "select " + label + ", " +
-                    "floor(avg(P.pylZawieszonyPm10)*100+0.5)/100.0, " +
-                    "floor(avg(P.tlenekWegla)*100+0.5)/100.0, " +
-                    "floor(avg(P.dwutlenekAzotu)*100+0.5)/100.0, " +
-                    "floor(avg(P.tlenekAzotu)*100+0.5)/100.0, " +
-                    "floor(avg(P.tlenkiAzotu)*100+0.5)/100.0, " +
-                    "floor(avg(P.pylZawieszonyPm25)*100+0.5)/100.0, " +
-                    "floor(avg(P.tlenekWegla8H)*100+0.5)/100.0, " +
-                    "floor(avg(P.benzen)*100+0.5)/100.0, " +
-                    "floor(avg(P.dwutlenekSiarki)*100+0.5)/100.0, " +
-                    "floor(avg(P.ozon)*100+0.5)/100.0, " +
-                    "floor(avg(P.ozon8H)*100+0.5)/100.0 " +
-                    "from SmogEntity as P " +
-                    String.format("where Data between '%s' and '%s' and Stacja = %s ", from, to, station) +
-                    String.format("group by (strftime('%%s', Data) + 3600 * Godzina) / %d", secs) +
-                    "order by Data, Godzina";
-
-            Session s = HibernateSession.getSessionFactory().openSession();
-            Query q = s.createQuery(query);
-            q.setMaxResults(24);
-            q.setResultTransformer(Transformers.aliasToBean(SmogQueryResult.class));
-            List result = q.list();
-            s.close();
-
-            return result;
-        } else {
-
+        String label;
+        if (divhours < 24) { // hours
+            label = "P.godzina || ':00'";
+        } else { // days, month
+            label = "P.data";
         }
 
-//        try {
-//            lines = Files.readAllLines(Paths.get(String.format("data/smog/%s/csv/%s.csv", station, date)));
-//            List<String[]> separated = lines.stream().map(l -> (l + ";X").split(";")).collect(Collectors.toList());
-//
-//            String[] firstLine = separated.get(0);
-//
-//            HashMap<String, List<String>> data = new HashMap<>();
-//
-//            for (int i=0; i<firstLine.length - 1; i++) {
-//                final int finalI = i;
-//                List<String> values = separated.stream().map(l -> l[finalI]).collect(Collectors.toList());
-//                data.put(firstLine[i], values.subList(1, values.size()));
-//            }
-//            return data;
-//
-//        } catch (IOException e) {
-//            System.err.println("Probably no file was found.");
-//            //e.printStackTrace();
-//        }
-//        return null;
+        String query = "select " + label + ", " +
+                "floor(avg(P.pylZawieszonyPm10)*100+0.5)/100.0, " +
+                "floor(avg(P.tlenekWegla)*100+0.5)/100.0, " +
+                "floor(avg(P.dwutlenekAzotu)*100+0.5)/100.0, " +
+                "floor(avg(P.tlenekAzotu)*100+0.5)/100.0, " +
+                "floor(avg(P.tlenkiAzotu)*100+0.5)/100.0, " +
+                "floor(avg(P.pylZawieszonyPm25)*100+0.5)/100.0, " +
+                "floor(avg(P.tlenekWegla8H)*100+0.5)/100.0, " +
+                "floor(avg(P.benzen)*100+0.5)/100.0, " +
+                "floor(avg(P.dwutlenekSiarki)*100+0.5)/100.0, " +
+                "floor(avg(P.ozon)*100+0.5)/100.0, " +
+                "floor(avg(P.ozon8H)*100+0.5)/100.0 " +
+                "from SmogEntity as P " +
+                String.format("where Data between '%s' and '%s' and Stacja = %s ", from, to, station) +
+                String.format("group by (strftime('%%s', Data) + 3600 * Godzina) / %d", secs) +
+                "order by Data, Godzina";
 
-        return null;
+        Session s = HibernateSession.getSessionFactory().openSession();
+        Query q = s.createQuery(query);
+        q.setMaxResults(24);
+        q.setResultTransformer(Transformers.aliasToBean(SmogQueryResult.class));
+        List result = q.list();
+        s.close();
+
+        return result;
     }
 }
--- src/main/java/SmogProject/WeatherController.java
+++ src/main/java/SmogProject/WeatherController.java
@@ -26,48 +26,40 @@ public class WeatherController {
 
     @RequestMapping("/weather")
     public @ResponseBody List weather(@RequestParam(value="from", defaultValue="2016-04-05") String from,
-                                      @RequestParam(value="to", defaultValue="2016-04-05") String to,
-                                      @RequestParam(value="view", defaultValue="normal") String view) throws ParseException {
+                                      @RequestParam(value="to", defaultValue="2016-04-05") String to) throws ParseException {
 
-        if (Objects.equals(view, "normal")) {
-            Date fromdate = new SimpleDateFormat("yyyy-MM-dd").parse(from);
-            Date todate = new SimpleDateFormat("yyyy-MM-dd").parse(to);
+        Date fromdate = new SimpleDateFormat("yyyy-MM-dd").parse(from);
+        Date todate = new SimpleDateFormat("yyyy-MM-dd").parse(to);
 
-            long diff = todate.getTime() - fromdate.getTime() + TimeUnit.DAYS.toMillis(1);
-            long secs = TimeUnit.MILLISECONDS.toSeconds(diff) / MAX_POINTS;
-            long divhours = TimeUnit.MILLISECONDS.toHours(diff) / MAX_POINTS + 1;
+        long diff = todate.getTime() - fromdate.getTime() + TimeUnit.DAYS.toMillis(1);
+        long secs = TimeUnit.MILLISECONDS.toSeconds(diff) / MAX_POINTS;
+        long divhours = TimeUnit.MILLISECONDS.toHours(diff) / MAX_POINTS + 1;
 
-            String label;
-            if (divhours < 24) { // hours
-                label = "P.godzina || ':00'";
-            } else { // days, month
-                label = "P.data";
-            }
-
-            String query = "select " + label + ", " +
-                           "avg(P.tempPowietrzaavg), " +
-                           "avg(P.avgPredkoscWiatruavg), " +
-                           "avg(P.opadavg), " +
-                           "avg(P.wilgotnoscavg), " +
-                           "avg(P.cisnienieavg) " +
-                           "from PogodaEntity as P " +
-                           String.format("where Data between '%s' and '%s' ", from, to) +
-                           String.format("group by (strftime('%%s', Data) + 3600 * Godzina) / %d", secs) +
-                           "order by Data, Godzina";
-
-            Session s = HibernateSession.getSessionFactory().openSession();
-            Query q = s.createQuery(query);
-            q.setMaxResults(24);
-            q.setResultTransformer(Transformers.aliasToBean(PogodaQueryResult.class));
-            List result = q.list();
-            s.close();
-
-            return result;
-        } else if (Objects.equals(view, "daynight")) {
-
+        String label;
+        if (divhours < 24) { // hours
+            label = "P.godzina || ':00'";
+        } else { // days, month
+            label = "P.data";
         }
 
+        String query = "select " + label + ", " +
+                       "avg(P.tempPowietrzaavg), " +
+                       "avg(P.avgPredkoscWiatruavg), " +
+                       "avg(P.opadavg), " +
+                       "avg(P.wilgotnoscavg), " +
+                       "avg(P.cisnienieavg) " +
+                       "from PogodaEntity as P " +
+                       String.format("where Data between '%s' and '%s' ", from, to) +
+                       String.format("group by (strftime('%%s', Data) + 3600 * Godzina) / %d", secs) +
+                       "order by Data, Godzina";
 
-        return null;
+        Session s = HibernateSession.getSessionFactory().openSession();
+        Query q = s.createQuery(query);
+        q.setMaxResults(24);
+        q.setResultTransformer(Transformers.aliasToBean(PogodaQueryResult.class));
+        List result = q.list();
+        s.close();
+
+        return result;
     }
 }
